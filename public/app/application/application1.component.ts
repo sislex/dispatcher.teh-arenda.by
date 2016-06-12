b2df752639a053c0e1059a582d349426c6987555
sislex <sislex@ya.ru>
2016-06-13 00:46:12 +0300

    added 2 directives

---
+++ public/app/application/application1.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: '[application-component]',
+    templateUrl: 'app/application/application.html'
+})
+export class ApplicationComponent { }
