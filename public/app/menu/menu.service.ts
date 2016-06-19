d56576edb39f1d8184de0950878361218529bbf5
sislex <sislex@ya.ru>
2016-06-19 13:44:03 +0300

    http

--- public/app/menu/menu.service.ts
+++ public/app/menu/menu.service.ts
@@ -1,20 +1,32 @@
+import { Injectable }    from '@angular/core';
 import { Headers, Http } from '@angular/http';
 
+import 'rxjs/add/operator/toPromise';
+@Injectable()
 export class MenuService {
-    constructor(){
-        this.getMenuFromCloud();
+    constructor(private http: Http){
+        this.getMenuFromCloud().then(heroes => {console.log(heroes)});
+
     }
 
     private url = 'api/getCategories';  // URL to web api
     public selectedMenu = '123';
 
-    getMenuFromCloud() {
-        //return this.http.get(this.url);
+    private getMenuFromCloud(): Promise  {
+        return this.http.get(this.url)
+            .toPromise()
+            .then(response => response.json())
+            .catch(this.handleError);
     }
 
-    getMenu(): string[] {
+    private getMenu(): string[] {
         // dummy data; should be replaced with connection to backend
         let dummyMenu = ['Экскаваторы','Погрузчики','Трактора','Бульдозеры','Краны'];
         return dummyMenu;
+    }
+
+    private handleError(error: any) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
     }
 }
